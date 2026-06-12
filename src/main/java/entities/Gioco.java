948a23789ef2e54a9826aa72ed0cdf0bb5c031c0
Vidied <davidepan2001@gmail.com>
2026-06-12 11:12:28 +0200

    classe astratta gioco con costruttore e getter

---
+++ src/main/java/entities/Gioco.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.util.Random;
+
+public abstract class Gioco {
+    //Random static e final perchè viene usato per ogni generazione e non cambia
+    private static final Random random = new Random();
+    //Attributi del gioco generale
+    private int id;
+    private String title;
+    private int anno;
+    private int prezzo;
+
+    public Gioco (String title, int anno, int prezzo) {
+        this.id = random.nextInt(10000, 100000);
+        this.title = title;
+        this.anno = anno;
+        this.prezzo = prezzo;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
+}
